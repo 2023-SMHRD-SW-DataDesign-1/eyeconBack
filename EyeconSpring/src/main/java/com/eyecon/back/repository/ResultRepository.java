a954a8dcd3c08eca7c7844cd0bd14ec2a0021fbd
ldy28 <28dlehd@gmail.com>
2023-11-01 23:43:29 +0900

    1101_11:42_이동영 beforeimage(히트맵) URL DB에 저장 기능 추가

--- EyeconSpring/src/main/java/com/eyecon/back/repository/ResultRepository.java
+++ EyeconSpring/src/main/java/com/eyecon/back/repository/ResultRepository.java
@@ -1,9 +1,18 @@
 package com.eyecon.back.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.eyecon.back.dto.ResultDTO;
 import com.eyecon.back.entity.Result;
+
+
 
 public interface ResultRepository extends JpaRepository<Result, Long>{
 
+	
+	Optional<Result> findByEmail(String email);
+	Optional<Result> findFirstByEmailOrderByIdDesc(String email);
 }
